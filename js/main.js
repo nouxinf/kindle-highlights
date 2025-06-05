004b0826c597f72f4b180d7a5b0598b6ef96c64b
nouxinf <bingchilling1915@gmail.com>
2025-06-05 20:02:15 +0100

    fixed typo (darkmode.js to darkMode.js)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { handleFileUpload, displayClippings, parsedClippings } from './clippings.js';
-import { applySystemDarkMode, setupDarkModeToggle } from './darkmode.js';
+import { applySystemDarkMode, setupDarkModeToggle } from './darkMode.js';
 import { setupHelp, fetchHelp } from "./help.js";
 document.getElementById('fileInput').addEventListener('change', handleFileUpload);
 document.getElementById('sortOrder').addEventListener('change', displayClippings);
